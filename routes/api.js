bb971caf54caa34da58d9c357fa589a7d8772bbb
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-05-06 15:26:33 -0500

    Worked on the api

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ router.get("/", (req, res) => {
 
 router.get("/todos", (req, res) => {
   // Send back the full list of items
+
   db("SELECT * FROM items ORDER BY id ASC;")
     .then(results => {
       res.send(results.data);
@@ -15,25 +16,39 @@ router.get("/todos", (req, res) => {
     .catch(err => res.status(500).send(err));
 });
 
-router.post("/todos", (req, res) => {
+router.post("/todos", async (req, response) => {
   // The request's body is available in req.body
   // If the query is successfull you should send back the full list of items
   // Add your code here
-  //
+  let text = req.body.text;
+  await db(`INSERT INTO items (text, complete) VALUES ("${text}", 0);`);
+  response.send(await db("SELECT * FROM items ORDER BY id DESC LIMIT 1;"));
 });
 
-router.put("/todos/:todo_id", (req, res) => {
+// router.post('/todoss', (req, response) => {
+//   let text = req.body.text;
+//   db(`INSERT INTO items (text, complete) VALUES ("${text}", 0);`).then(() => {
+//     db('SELECT * FROM items ORDER BY id ASC;').then((results) => {
+//       response.send(results.data);
+//     });
+//   });
+// });
+
+router.put("/todos/:todo_id", async (req, response) => {
   // The request's body is available in req.body
   // URL params are available in req.params
   // If the query is successfull you should send back the full list of items
   // Add your code here
-  //
+  let newText = req.body.text;
+  let id = req.params.todo_id;
+  await db(`UPDATE items SET text = "${newText}" WHERE id = ${id}`);
+  response.send(await db(`SELECT * FROM items WHERE id = ${id};`));
 });
 
-router.delete("/todos/:todo_id", (req, res) => {
-  // URL params are available in req.params
-  // Add your code here
-  //
+router.delete("/todos/:todo_id", async (req, response) => {
+  let id = req.params.todo_id;
+  await db(`DELETE FROM items WHERE id = ${id};`);
+  response.status(200).send();
 });
 
 module.exports = router;
